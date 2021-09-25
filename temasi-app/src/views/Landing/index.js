87822985ca670c3f55e239a90efa37d4526bb56b
ibrahimsyah <ibra.himsyah10@gmail.com>
2021-09-25 21:46:13 +0700

    configure eslintrc

--- temasi-app/src/views/Landing/index.js
+++ temasi-app/src/views/Landing/index.js
@@ -1,32 +1,32 @@
 import { useNavigation, StackActions } from '@react-navigation/native';
-import React from 'react'
-import { Image, Text, View, StatusBar } from 'react-native'
-import Carousel from 'react-native-looped-carousel'
+import React from 'react';
+import { Image, Text, View, StatusBar } from 'react-native';
+import Carousel from 'react-native-looped-carousel';
 import ButtonPrimary from '../../components/ButtonPrimary';
 import { APP_FIRST_USE } from '../../configs/storage';
 import storage from '../../providers/storage';
-import config from './index.config'
-import style from './style'
+import config from './index.config';
+import style from './style';
 
 const LandingItem = props => {
-    const { image, title, description } = props
+    const { image, title, description } = props;
     return <>
         <View style={style.landingItemContainer}>
             <Image source={image} style={style.landingImage} />
             <Text style={style.landingTitle}>{title}</Text>
             <Text style={style.landingDesc}>{description}</Text>
         </View>
-    </>
-}
+    </>;
+};
 
 export default () => {
-    const navigation = useNavigation()
+    const navigation = useNavigation();
 
     const onButtonClick = () => {
-        storage.setData(APP_FIRST_USE, false)
-        const navAction = StackActions.replace('HomeScreen')
-        navigation.dispatch(navAction)
-    }
+        storage.setData(APP_FIRST_USE, false);
+        const navAction = StackActions.replace('HomeScreen');
+        navigation.dispatch(navAction);
+    };
 
     return <>
         <StatusBar {...config.statusBarStyle} />
@@ -36,5 +36,5 @@ export default () => {
             </Carousel>
             <ButtonPrimary onClick={onButtonClick}>Lanjutkan</ButtonPrimary>
         </View>
-    </>
-}
+    </>;
+};
