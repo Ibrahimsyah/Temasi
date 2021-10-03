7aa497c5092138bef12325f92213c687d81cbd8e
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-10-03 21:59:20 +0700

    add location enabler

--- temasi-app/src/views/Main/index.js
+++ temasi-app/src/views/Main/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, {useEffect} from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import RNAndroidLocationEnabler from 'react-native-android-location-enabler';
 import config from './index.config';
-import { StatusBar } from 'react-native';
-import { Color } from '../../configs/style';
+import {StatusBar} from 'react-native';
+import {Color} from '../../configs/style';
 
 const Tab = createBottomTabNavigator();
 
 export default () => {
+
+  useEffect(() => {
+    RNAndroidLocationEnabler.promptForEnableLocationIfNeeded({
+      interval: 10000,
+      fastInterval: 5000,
+    }).then(data => {
+      console.log(data);
+    });
+  }, []);
   return (
     <>
       <StatusBar backgroundColor={Color.LIGHT_GRAY} barStyle="dark-content" />
