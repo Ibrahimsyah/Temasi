fcce0582bccc972bb03834167a593b7b7d8dd2aa
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-11-16 17:18:56 +0700

    [App] Add permohonan stores

--- temasi-app/src/store/permohonan.action.js
+++ temasi-app/src/store/permohonan.action.js
@@ -1,8 +1,42 @@
-import { SUBMIT_PERMOHONAN } from './ActionTypes';
+import {
+  SET_LATEST_PERMOHONAN,
+  SET_SEARCH_PERMOHONAN_RESULT,
+  SET_SELF_PERMOHONAN,
+  SET_URGENT_PERMOHONAN,
+  SUBMIT_PERMOHONAN,
+} from './ActionTypes';
 
 export const submitPermohonan = payload => {
   return {
     type: SUBMIT_PERMOHONAN,
+    payload,
+  };
+};
+
+export const setSelfPermohonan = payload => {
+  return {
+    type: SET_SELF_PERMOHONAN,
+    payload,
+  };
+};
+
+export const setLatestPermohonan = payload => {
+  return {
+    type: SET_LATEST_PERMOHONAN,
+    payload,
+  };
+};
+
+export const setUrgentPermohonan = payload => {
+  return {
+    type: SET_URGENT_PERMOHONAN,
+    payload,
+  };
+};
+
+export const setSearchPermohonanResult = payload => {
+  return {
+    type: SET_SEARCH_PERMOHONAN_RESULT,
     payload,
   };
 };
---
+++ temasi-app/src/store/permohonan.reducer.js
@@ -0,0 +1,43 @@
+import {
+  SET_LATEST_PERMOHONAN,
+  SET_SEARCH_PERMOHONAN_RESULT,
+  SET_SELF_PERMOHONAN,
+  SET_URGENT_PERMOHONAN,
+} from './ActionTypes';
+
+const initState = {
+  latest: [],
+  mostUrgent: [],
+  searchResult: [],
+  self: [],
+};
+
+export default (state = initState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case SET_LATEST_PERMOHONAN: {
+      return {
+        ...state,
+        latest: payload,
+      };
+    }
+    case SET_SELF_PERMOHONAN: {
+      return {
+        ...state,
+        self: payload,
+      };
+    }
+    case SET_SEARCH_PERMOHONAN_RESULT: {
+      return {
+        ...state,
+        searchResult: payload,
+      };
+    }
+    case SET_URGENT_PERMOHONAN: {
+      return {
+        ...state,
+        mostUrgent: payload,
+      };
+    }
+  }
+};
