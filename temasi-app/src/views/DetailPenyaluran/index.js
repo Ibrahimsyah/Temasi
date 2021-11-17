1cf6f5e392d7e72575d8ac9954416b1dc56cc569
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-11-17 22:30:21 +0700

    [App] bind detail penyaluran

--- temasi-app/src/views/DetailPenyaluran/index.js
+++ temasi-app/src/views/DetailPenyaluran/index.js
@@ -1,6 +1,6 @@
+import React, { useEffect, useMemo } from 'react';
 import { useRoute } from '@react-navigation/core';
-import React from 'react';
-import { Text, ScrollView, View, StatusBar } from 'react-native';
+import { Text, ScrollView, View, StatusBar, Image } from 'react-native';
 
 import Header from '../../components/Header';
 import PermohonanDetail from '../../components/PermohonanDetail';
@@ -9,11 +9,30 @@ import { Color } from '../../config/style';
 
 import style from './style';
 import styles from './style';
+import { useDispatch, useSelector } from 'react-redux';
+import { getDonaturPermohonanDetail } from '../../store/permohonan.action';
+import { absoluteUrl } from '../../utils/asset';
 
 export default () => {
   const router = useRoute();
+  const { permohonan } = useSelector(state => state);
+  const dispatch = useDispatch();
 
   const { time, ...permohonanDetailData } = router.params;
+  const id = useMemo(() => permohonanDetailData.id, [permohonanDetailData]);
+
+  useEffect(() => {
+    dispatch(
+      getDonaturPermohonanDetail({
+        permohonanId: id,
+      }),
+    );
+  }, [id, dispatch]);
+
+  useEffect(() => {
+    console.log(permohonan.donaturDetail);
+  }, [permohonan]);
+
   return (
     <>
       <StatusBar backgroundColor={Color.WHITE} />
@@ -22,8 +41,13 @@ export default () => {
         <PermohonanDetail {...permohonanDetailData} />
         <Text style={style.titleMed}>Detail Donatur</Text>
         <View style={style.donaturProfile}>
-          <View style={style.donaturPic} />
-          <Text style={style.donaturName}>John Doe</Text>
+          <Image
+            source={{ uri: absoluteUrl(permohonan.donaturDetail.photo) }}
+            style={style.donaturPic}
+          />
+          <Text style={style.donaturName}>
+            {permohonan.donaturDetail.full_name}
+          </Text>
         </View>
         <View style={style.tutorialContainer}>
           <Text>
